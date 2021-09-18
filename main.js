56e1f93dd288e99a30fe01a79bd44f565af41aa1
Antonio-David-Tejada-Villalon <4vdel777@gmail.com>
2021-09-18 16:46:35 -0300

    Second Commit Completing Associative Array

--- main.js
+++ main.js
@@ -22,7 +22,39 @@ const links = [
     {
           label: "Week6 notes",
           url: "week6/index.html"
-    }
+    },
+    {
+      label: "Week7 notes",
+      url: "week7/index.html"
+},
+{
+  label: "Week8 notes",
+  url: "week8/index.html"
+},
+{
+  label: "Week9 notes",
+  url: "week9/index.html"
+},
+{
+  label: "Week10 notes",
+  url: "week10/index.html"
+},
+{
+  label: "Week11 notes",
+  url: "week11/index.html"
+},
+{
+  label: "Week12 notes",
+  url: "week12/index.html"
+},
+{
+  label: "Week13 notes",
+  url: "week13/index.html"
+},
+{
+  label: "Week14 notes",
+  url: "week14/index.html"
+}
   ]
 
   
